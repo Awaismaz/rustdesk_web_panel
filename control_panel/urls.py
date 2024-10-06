bf44c3a0723e0810fd6fee830886cdb1d455f2ca
Awaismaz <awais_mazahir@hotmail.com>
2024-10-06 15:49:24 +0500

    updated

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  # Dashboard linked to the home URL
@@ -8,3 +10,6 @@ urlpatterns = [
     path('domains/', views.manage_domains, name='manage_domains'),
     path('api/lock_screen/', views.get_active_lock_screen, name='get_active_lock_screen'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
